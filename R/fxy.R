3bcf1e5f2ac740f275f0528aa6109204e13746e6
dy <jealousing@gmail.com>
2018-03-31 03:47:14 +0800

    debug,profile,Adam

--- R/fxy.R
+++ R/fxy.R
@@ -1,49 +1,81 @@
 fxy <- function(X, Y, potentials = list (
-    list(word = dtm(ngram = 1:1, weight = 0L), y = NA, value = 1)
+    list(word = dtm(ngram = 1:1, weight = 0L), y = NA, score = 1)
 ), lower.f = 1L, lower.d = 1L) {
+
+    stopifnot(is.vector(X) && sum(sapply(X, is.character)) == length(X))
+    stopifnot(is.atomic(Y))
+    stopifnot(length(X) == length(Y))
+    # todo check NA of X and Y
+    lower.f = as.integer(lower.f)
+    lower.d = as.integer(lower.d)
+    class.Y = class(Y)
     Y = as.factor(Y)
     levels.Y = levels(Y)
     Y = as.integer(Y)
     np = length(potentials)
     xs = list()
-    ws = list()
-    for (p in potentials) {
-        predicate = p[[1L]]
-        y = p[[2L]]
+    ss = list()
+    np = length(potentials)
+    predicates = vector("list", np)
+    ys = vector("numeric", np)
+    scores = vector("numeric", np)
+    ips = 1:np
+    for (ip in ips) {
+        p = potentials[[ip]]
+        predicate = predicates[[ip]] = p[[1L]]
+        y = ys[ip] = p[[2L]]
         if (is.na(y))
             y = 1:length(levels.Y)
         else {
+            stopifnot(class(y) == class.Y)
             y = as.integer(factor(p[[2L]], levels = levels.Y))
             if (is.na(y))
-                stop(paste("Value of feature Function outcome [", P[[2L]], "] not in given levels [",
-                           paste(levels.Y, sep = ", "), "]."))
+                stop(paste0("y of potential functions contains class-tag not belongs to Y, i.e, ", P[[2L]], " not in [", paste(levels.Y, sep = ", "), "]."))
         }
-        value = ifelse(is.null(p$value), 1, p$value)
         SM = as(predicate(X), "dgCMatrix") #todo
         prefix = names(p)[[1L]]
         colnames(SM) = str_c(prefix, colnames(SM), sep = "_")
         if (!is.null(xs$prefix))
             stop(paste("Duplicated Feature Functions(name): ", prefix))
         xs$prefix = SM
-        W = matrix(0, nrow = ncol(SM), ncol = length(levels.Y))
-        W[, y] = 1
-        ws$prefix = W
+        S = matrix(0, nrow = ncol(SM), ncol = length(levels.Y))
+        score = scores[ip] = ifelse(is.null(p$score), 1, p$score) #todo how to use
+        S[, y] = score
+        ss$prefix = S
     }
     ans = list()
     FX = do.call(cbind, xs)
-    FW = do.call(cbind, ws)
-    validWi = (lower.f <= rowSums(FW))
-    ans$features = c(sum(validWi), nrow(FW), lower.f)
-    FW = FW[validWi,, drop = F]
+    FS = do.call(cbind, ss)
+    validWi = (lower.f <= Matrix::colSums(FX))
+    ans$features = c(sum(validWi), nrow(FS), lower.f)
+    FS = FS[validWi,, drop = F]
     FX = FX[, validWi, drop = F]
     validXi = (lower.d <= Matrix::rowSums(FX))
     FX = FX[validXi,, drop = F]
     ans$Y = Y[validXi]
     ans$inputs = c(sum(validXi), length(X), lower.d)
-    ans$X = FX
-    ans$W = FW
+    ans$FX = FX
+    ans$FS = FS
     ans$levels.Y = levels.Y
-    ans$potentials = potentials
+    ans$potentials = data.table(predicate = predicates, y = ys, score = scores)
+    nx = nrow(FX)
+    np = ncol(FX)
+    stopifnot(ncol(FX) == nrow(FS))
+    nc = ncol(FS)
+    nf = np * nc
+    ans$nx = nx; ans$np = np; ans$nc = nc; ans$nf = nf;
+    iNozeros = data.table(Matrix::which(FX != 0, arr.ind = T))
+    adjDfm = iNozeros[, list(adj = list(col)), keyby = row]$adj
+    adjFdm = iNozeros[, list(adj = list(row)), keyby = col]$adj
+    actualMean = matrix(0, nrow = np, ncol = nc)
+    for (ip in 1:np) {
+        ys = Y[adjFdm[[ip]]]
+        for (y in ys)
+            actualMean[ip, y] = actualMean[ip, y] + 1
+    }
+    ans$adjDfm = adjDfm
+    ans$adjFdm = adjFdm
+    ans$actualMean = actualMean
     ans$call = match.call()
     class(ans) = "fxy"
     ans
--- R/me.R
+++ R/me.R
@@ -1,101 +1,91 @@
-me <- function(X, Y, niters = 100L, verbose = TRUE,
-               predicates = list(
-                   function(X, Y, i) str_detect(X[[i]], "澳门"),
-                   function(X, Y, i) str_detect(X[[i]], "余额"),
-                   function(X, Y, i) str_detect(X[[i]], "OX")
-               ),
-               y = c(TRUE, FALSE, FALSE),
-               lower = 1L, upper = .Machine$integer.max
-) {
-    stopifnot(is.vector(X) && sum(sapply(X, is.character)) == length(X))
-    stopifnot(class(Y) == class(y) && is.atomic(Y))
-    stopifnot(length(X) == length(Y))
-    stopifnot(length(predicates) == length(y))
-    if (sum(sapply(X, is.null)) > 0L || sum(sapply(Y, is.null)) > 0L
-        || sum(sapply(X, is.na)) > 0L || sum(sapply(Y, is.na)) > 0L) {
-        stop("Input data contains either NA or NULL.")
-    }
-    if (sum(sapply(y, is.null)) > 0L || sum(sapply(y, is.null)) > 0L)
-        stop("y of feature functions contain either NA or NULL. You can use file input to illustrate\n
-             class-unrelated predicates.")
-    Y = as.factor(Y)
-    levels.Y = levels(Y)
-    Y = as.integer(Y)
-    y = as.integer(factor(y, levels = levels.Y))
-    if (sum(sapply(y, is.na)) > 0L)
-        stop("y of potential functions contains class-tag not belongs to Y.")
-    if (0 < lower && lower < 1) lower = as.integer(n * lower)
-    if (0 < upper && upper < 1) upper = as.integer(n * upper)
-
-    n = length(X); m = length(levels.Y)
-    np = length(predicates)
-    # weight = vector("numeric", np)
-    weight = rnorm(np) # todo
-    P = matrix(0, nrow = n, ncol = m) # probability
-    M = vector("list", n)
-    actualMean = rep(0, np)
-    maxentMean = rep(0, np)
-    whichP = rep(TRUE, np)
-    ips = 1:np
-    ixs = 1:n
-    for (ix in ixs) {
-        for (ip in ips) {
-            whichP[ip] = predicates[[ip]](X, Y, ix)
-            if (whichP[ip] && y[ip] == Y[ix])
-                actualMean[ip] = actualMean[ip] + 1
-        }
-        M[[ix]] = ips[whichP]
-    }
-    valid <- function(ip) {
-        lower <= actualMean[ip] & actualMean[ip] < upper
-    }
-    for (ix in ixs) {
-        M[[ix]] = M[[ix]][valid(M[[ix]])]
-    }
-    ips = ips[valid(ips)]
-    losses = rep(NA, niters)
-    it = 1L
+me <- function(X, niters = 100L, verbose = TRUE, R.optim = FALSE, nBatches = 2L) {
+    library(data.table)
+    stopifnot(class(X) == "fxy")
+    Y = X$Y; FX = X$FX; FS = X$FS
+    nx = X$nx; nc = X$nc; np = X$np; nf = X$nf
+    ixs = 1:nx; ips = 1:np
+    adjDfm = X$adjDfm; adjFdm = X$adjFdm
+    actualMean = X$actualMean
+    model <- new.env(parent = emptyenv())
+    P = matrix(0, nrow = nx, ncol = nc, dimnames = list(NULL, X$levels.Y)) # probability
+    FW = matrix(rnorm(nf), nrow = np, ncol = nc, dimnames = list(colnames(FX),  X$levels.Y))
+    FW[FS == 0] = 0
+    model$losses = rep(NA, niters)
+    model$it = 1L
     loss <- function(weight) {
-        lhs = 0; rhs = 0
+        FW = matrix(weight, nrow = np, ncol = nc)
+        lhs = 0
+        rhs = 0
         for (ix in ixs) {
-            for (ip in M[[ix]]) {
-                yip = y[ip]
-                P[ix, yip] = P[ix, yip] + weight[yip]
-                if (yip == Y[ix])
-                    rhs = rhs + weight[yip]
-            }
+            ifs = adjDfm[[ix]]
+            lse = logsumexp(colSums(FS[ifs,, drop = F] * FW[ifs,, drop = F])) #todo
+            lhs = lhs + lse
+            rhs = rhs + sum(FS[ifs, Y[ix]] * FW[ifs, Y[ix]])
         }
-        lhs = log(rowSums(exp(P))) #todo
-        ans = sum(lhs) - rhs
-        losses[it] = ans
-        it = it + 1L
-        # print(paste("loss: ", ans))
-        ans
+        curLoss = (lhs - rhs) / nx
+        model$losses[model$it] = curLoss
+        print(paste(model$it, ":", curLoss))
+        model$it = model$it + 1L
+        curLoss
     }
     grad <- function(weight) {
+        FW = matrix(weight, nrow = np, ncol = nc)
         for (ix in ixs) {
-            for (ip in M[[ix]]) {
-                yip = y[ip]
-                P[ix, yip] = P[ix, yip] + weight[yip]
-            }
+            ifs = adjDfm[[ix]]
+            P[ix,] = colSums(FS[ifs,, drop = F] * FW[ifs,, drop = F]) #todo
+            P[ix,] = exp(P[ix,] - logsumexp(P[ix,]))
         }
-        P[,] = exp(P)
-        P[,] = P / rowSums(P) #todo
-        maxentMean[] = 0
-        for (ix in ixs) {
-            for (ip in ips) {
-                maxentMean[ip] = maxentMean[ip] + P[ix, y[ip]]
-            }
+        # P[,] = exp(P)
+        # P[,] = P / Matrix::rowSums(P)
+        # P[,] = P / rowSums(P) #todo necessary?
+        for (ip in ips) {
+            ids = adjFdm[[ip]]
+            FW[ip,] = colSums(P[ids,, drop = F])
         }
-        ans = maxentMean - actualMean
-        # print(paste("grad: ", ans))
-        ans
+        FW[,] = (FW * FS - actualMean) / nx
+        as.double(FW) # todo
     }
-    opt.obj = optim(weight, fn = loss, gr = grad, method = "L-BFGS", control = list(
-        trace = ifelse(verbose, 10L, 0L), maxit = niters))
-    ans = list(opt.obj = opt.obj, predicates = predicates, y = y,
-               ips = ips, M = M, losses = losses,
-               levels.Y = levels.Y, call = match.call())
+    # check.grad(loss, grad, FW, epsilon = 1e-9)
+    if (R.optim) {
+        opt.obj = optim(as.double(FW), fn = loss, gr = grad, method = "L-BFGS", control = list(
+            trace = ifelse(verbose, 10L, 0L), maxit = niters))
+        FW[,] = opt.obj$par
+    } else {
+        # Adam
+        lambda = 0.1
+        beta.mean = 0.9
+        beta.var = 0.999
+        epsilon = 1e-8
+        mean.grad = rep(0, nf)
+        var.grad = rep(0, nf)
+        make.batches <- function(x, nBatches = 10L) {
+            stopifnot(is.integer(x))
+            ans = vector("list", nBatches)
+            if (length(x) == 1L)  {
+
+            }
+        }
+        batches = make.batches(nx, nBatches)
+        for (it in 1:niters) {
+            if (it == 1 || (it - 1) %% nBatches == 0) {
+                loss(as.double(FW))
+            }
+            for (ixs in batches) {
+                for (ix in ixs) {
+                    ifs = adjDfm[[ix]]
+                    P[ix,] = colSums(FS[ifs,, drop = F] * FW[ifs,, drop = F]) #todo
+                    P[ix,] = exp(P[ix,] - logsumexp(P[ix,]))
+                }
+                cur.grad = grad(as.double(FW))
+                mean.grad[] = beta.mean * mean.grad + (1 - beta.mean) * cur.grad
+                mean.var[]  = beta.var * mean.var + (1 - beta.var) * cur.grad * cur.grad
+                FW[,] = FW - lambda * mean.grad / (sqrt(mean.var) + epsilon)
+            }
+        }
+    }
+    stopifnot(all(FW[FS == 0] == 0))
+    ans = list(FW = FW, P = P, losses = na.omit(model$lossess),
+               predicates = X$potentials$predicate, call = match.call())
     class(ans) = "me"
     ans
 }
@@ -123,4 +113,49 @@ predict.me <- function(obj, X, ...) {
 
 plot.me <- function(obj, ...) {
     plot(obj$losses, xlab = "Iteration", ylab = "Negative Log Likelihood")
+}
+
+check.grad <- function(fun, grad, weight, epsilon = 1e-4) {
+    weight = as.double(weight)
+    testGrad = grad(weight)
+    trueGrad = vector("numeric", length(weight))
+    for (i in 1:length(weight)) {
+        weight[i] = weight[i] - epsilon
+        v1 = fun(weight)
+        weight[i] = weight[i] + 2 * epsilon
+        v2 = fun(weight)
+        trueGrad[i] = (v2 - v1) / epsilon / 2
+        c = all.equal(trueGrad[i], testGrad[i], tolerance = epsilon)
+        if (!isTRUE(c)) {
+            s = c(trueGrad[i], testGrad[i])
+            names(s) = c("True", "Test")
+            print(s)
+            warning(paste0(as.character(c), " at index ", i))
+        }
+        weight[i] = weight[i] - epsilon
+    }
+}
+
+logsumexp <- function(x) {
+    a = max(x)
+    a + log(sum(exp(x - a)))
+}
+
+test.me.1 <- function() {
+    X = list(c("A"), c("B"), c("A", "B"))
+    Y = c(T, F, F)
+    fxy.obj <<- fxy(X, Y)
+    print(fxy.obj)
+    me.obj <<- me(fxy.obj, verbose = FALSE)
+    print(me.obj)
+}
+
+test.me.2 <- function() {
+    spam.sms = na.omit(spam.sms)
+    X <<- tk(spam.sms$body)
+    Y <<- spam.sms$is.spam
+    fxy.obj <<- fxy(X, Y, lower.f = 3L)
+    print(fxy.obj)
+    me.obj <<- me(fxy.obj, verbose = FALSE)
+    print(me.obj)
 }
